90dcfa20bb3688ce9d6379b6d00b00326d162791
Nimesh Regmi <102865175+Nimeshregmi@users.noreply.github.com>
2024-07-16 13:11:36 +0545

    first changes

---
+++ HousePricePrediction/server/utils.py
@@ -0,0 +1,62 @@
+import json
+import pickle
+import numpy as np
+import os
+
+__locations = None
+__data_columns = None
+__model = None
+def get_estimated_price(locations, sqft, bhk, bath):
+    try:
+        loc_index = __data_columns.index(locations.lower())
+    except:
+        loc_index = -1
+    x = np.zeros(len(__data_columns))
+    x[0] = sqft
+    x[1] = bhk
+    x[2] = bath
+    if loc_index >= 0:
+        x[loc_index] = 1
+    return round(__model.predict([x])[0], 2)
+
+def get_location_name():
+    load_saved_artifacts()
+    return __locations
+
+def load_saved_artifacts():
+    print('Loading saved artifacts...start')
+    global __data_columns
+    global __locations
+    global __model
+    
+    # Print current working directory
+    print(f"Current working directory: {os.getcwd()}")
+    
+    # Construct the full path to columns.json
+    columns_file_path = './artifacts/city.json'
+    
+    
+    # Check if the file exists
+    if not os.path.exists(columns_file_path):
+        print(f"Error: {columns_file_path} does not exist.")
+        return
+    
+    # Load data_columns from columns.json
+    with open(columns_file_path, 'r') as f:
+        __data_columns = json.load(f)['data_columns']
+        __locations = __data_columns[3:]
+    
+    # Load the model from pickle file
+    with open('./artifacts/banglore_home_price_model.pickle', 'rb') as f:
+        __model = pickle.load(f)
+    
+    print('Loading saved artifacts...done')
+
+if __name__ == '__main__':
+    print(__locations)
+    load_saved_artifacts()
+    print(get_location_name())
+    print(get_estimated_price('1st Phase JP Nagar',1000, 3, 3))
+    print(get_estimated_price('1st Phase JP Nagar', 1000, 2, 2))
+    print(get_estimated_price('Kalhalli', 1000, 2, 2)) # other location
+    print(get_estimated_price('Ejipura', 1000, 2, 2))  # other location
